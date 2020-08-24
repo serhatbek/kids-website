439cf92f01cf6cc60813ec5a2324085f1d820e7f
serhatbek <serhat.bekk@gmail.com>
2020-08-24 22:40:48 +0300

    Last commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,10 @@
+const menu = document.querySelector('.menu');
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('change');
+  document.querySelector('.navbar').classList.toggle('change');
+});
+
 document.querySelectorAll('.card-btn').forEach((btn) => {
   btn.addEventListener('click', () => {
     btn.parentElement.classList.toggle('change');
